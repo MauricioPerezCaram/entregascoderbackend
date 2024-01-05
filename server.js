51753ba5788060d1f6a980b5c3c2906a7f40eb34
MauricioPerezCaram <perezcarammauricio@gmail.com>
2024-01-05 13:04:32 -0300

    listo crud products y users

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import products from "./data/fs/products.fs.js";
 import users from "./data/fs/users.fs.js";
+import { error } from "console";
 
 const server = express();
 
@@ -11,7 +12,7 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
 // PRODUCTS
-// Crear
+// Crear producto
 server.post("/api/products", async (req, res) => {
   try {
     const data = req.body;
@@ -24,7 +25,7 @@ server.post("/api/products", async (req, res) => {
     } else {
       return res.json({
         statusCode: 201,
-        message: "Creado",
+        message: "Producto creado",
         response,
       });
     }
@@ -62,7 +63,7 @@ server.get("/api/products", async (req, res) => {
   }
 });
 
-// Leer uno
+// Leer un producto
 server.get("/api/products/:pid", async (req, res) => {
   try {
     const { pid } = req.params;
@@ -87,17 +88,26 @@ server.get("/api/products/:pid", async (req, res) => {
   }
 });
 
-// Vender productos
+// Vender productos (disminuir stock)
 server.put("/api/products/:pid/:quantity", async (req, res) => {
   try {
     const { pid, quantity } = req.params;
     const response = await products.soldProduct(quantity, pid);
-    if (response) {
+    if (typeof response === "number") {
       return res.json({
         statusCode: 200,
         response: "Stock disponible " + response,
       });
+    } else if (response === "No existe ese producto") {
+      return res.json({
+        statusCode: 404,
+        message: error.message,
+      });
     } else {
+      return res.json({
+        statusCode: 400,
+        return: response,
+      });
     }
   } catch (error) {
     console.log(error);
@@ -108,7 +118,7 @@ server.put("/api/products/:pid/:quantity", async (req, res) => {
   }
 });
 
-// Eliminar productos
+// Eliminar producto
 server.delete("/api/products/:pid", async (req, res) => {
   try {
     const { pid } = req.params;
@@ -133,40 +143,105 @@ server.delete("/api/products/:pid", async (req, res) => {
   }
 });
 
-// // USERS
-// server.get("/api/users", (req, res) => {
-//   try {
-//     const all = users.readUsers();
-//     if (Array.isArray(all)) {
-//       return res.status(200).json({
-//         success: true,
-//         response: all,
-//       });
-//     } else {
-//       return res.status(404).json({
-//         success: false,
-//         message: all,
-//       });
-//     }
-//   } catch (error) {
-//     return res.status(500).json({
-//       success: false,
-//       message: error.message,
-//     });
-//   }
-// });
+// USERS
+// Crear usuario
+server.post("/api/users", async (req, res) => {
+  try {
+    const data = req.body;
+    const response = await users.createUsers(data);
+    if (response === "El nombre,la foto y el mail del usuario es necesario") {
+      return res.json({
+        statusCode: 400,
+        message: response,
+      });
+    } else {
+      return res.json({
+        statusCode: 201,
+        message: "Usuario creado",
+        response,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.json({
+      statusCode: 500,
+      message: error.message,
+    });
+  }
+});
 
-// server.get("/api/users/:uid", (req, res) => {
-//   try {
-//     const { uid } = req.params;
-//     const one = users.readUserById(uid);
-//     return res.status(200).json(one);
-//   } catch (error) {
-//     return res.status(500).json({
-//       success: false,
-//       message: error.message,
-//     });
-//   }
-// });
+// Leer usuarios
+server.get("/api/users", async (req, res) => {
+  try {
+    const all = await users.readUsers();
+    if (Array.isArray(all)) {
+      return res.json({
+        statusCode: 200,
+        response: all,
+      });
+    } else {
+      return res.json({
+        statusCode: 404,
+        message: all,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.json({
+      statusCode: 500,
+      message: error.message,
+    });
+  }
+});
+
+// Leer un usuario
+server.get("/api/users/:uid", async (req, res) => {
+  try {
+    const { uid } = req.params;
+    const one = await users.readUserById(uid);
+    if (typeof one === "string") {
+      return res.json({
+        statusCode: 404,
+        message: one,
+      });
+    } else {
+      res.json({
+        statusCode: 200,
+        response: one,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.json({
+      statusCode: 500,
+      message: error.message,
+    });
+  }
+});
+
+// Eliminar usuario
+server.delete("/api/users/:uid", async (req, res) => {
+  try {
+    const { uid } = req.params;
+    const response = await users.destroyUserById(uid);
+    if (response === "No hay usuario para borrar con el id ") {
+      return res.json({
+        statusCode: 404,
+        message: response,
+      });
+    } else {
+      return res.json({
+        statusCode: 200,
+        response,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.json({
+      statusCode: 500,
+      message: error.message,
+    });
+  }
+});
 
 server.listen(PORT, ready);
