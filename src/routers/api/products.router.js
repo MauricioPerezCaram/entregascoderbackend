33b44f27da70738202c278d8295a947760a0ffa4
MauricioPerezCaram <perezcarammauricio@gmail.com>
2024-01-05 17:28:57 -0300

    perfecto el product.router.js

--- src/routers/api/products.router.js
+++ src/routers/api/products.router.js
@@ -11,90 +11,59 @@ productsRouter.post("/", propsProducts, async (req, res, next) => {
     const response = await products.createProduct(data);
     return res.json({
       statusCode: 201,
-      message: "Producto creado",
       response,
     });
   } catch (error) {
     return next(error);
   }
 });
+
 productsRouter.get("/", async (req, res, next) => {
   try {
     const all = await products.readProducts();
-
-    if (Array.isArray(all)) {
-      return res.json({
-        statusCode: 200,
-        response: all,
-      });
-    } else {
-      return res.json({
-        statusCode: 404,
-        message: all,
-      });
-    }
+    return res.json({
+      statusCode: 200,
+      response: all,
+    });
   } catch (error) {
     return next(error);
   }
 });
+
 productsRouter.get("/:pid", async (req, res, next) => {
   try {
     const { pid } = req.params;
     const one = await products.readProductById(pid);
-    if (typeof one === "string") {
-      return res.json({
-        statusCode: 404,
-        message: one,
-      });
-    } else {
-      res.json({
-        statusCode: 200,
-        response: one,
-      });
-    }
+    return res.json({
+      statusCode: 200,
+      response: one,
+    });
   } catch (error) {
     return next(error);
   }
 });
-productsRouter.put("/:pid", async (req, res, next) => {
+
+productsRouter.put("/:pid/:quantity", async (req, res, next) => {
   try {
     const { pid, quantity } = req.params;
     const response = await products.soldProduct(quantity, pid);
-    if (typeof response === "number") {
-      return res.json({
-        statusCode: 200,
-        response: "Stock disponible " + response,
-      });
-    } else if (response === "No existe ese producto") {
-      return res.json({
-        statusCode: 404,
-        message: error.message,
-      });
-    } else {
-      return res.json({
-        statusCode: 400,
-        return: response,
-      });
-    }
+    return res.json({
+      statusCode: 200,
+      response: "Stock disponible: " + response,
+    });
   } catch (error) {
     return next(error);
   }
 });
+
 productsRouter.delete("/:pid", async (req, res, next) => {
   try {
     const { pid } = req.params;
     const response = await products.destroyProductById(pid);
-    if (response === "No hay producto para borrar con el id ") {
-      return res.json({
-        statusCode: 404,
-        message: response,
-      });
-    } else {
-      return res.json({
-        statusCode: 200,
-        response,
-      });
-    }
+    return res.json({
+      statusCode: 200,
+      response,
+    });
   } catch (error) {
     return next(error);
   }
