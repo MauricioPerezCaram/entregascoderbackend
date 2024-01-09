b4f94cf2f23ae841520ef79fdbbae668c835ddea
MauricioPerezCaram <perezcarammauricio@gmail.com>
2024-01-09 14:52:55 -0300

    min 35.24

--- src/routers/index.router.js
+++ src/routers/index.router.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import apiRouter from "./api/index.router.js";
+import viewsRouter from "./views/index.router.js";
 
 const router = Router();
 
-// falta implementar el router de vistas
 router.use("/api", apiRouter);
+router.use("/", viewsRouter);
 
 export default router;
