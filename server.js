bfd537bc8bd2504fae604b89a12e00aedc1ae1a9
MauricioPerezCaram <perezcarammauricio@gmail.com>
2024-01-09 20:00:15 -0300

    motores de plantillas post break

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 import express from "express";
+import morgan from "morgan";
+import { engine } from "express-handlebars";
+
 import router from "./src/routers/index.router.js";
 import errorHandler from "./src/middlewares/errorHandler.mid.js";
 import pathHandler from "./src/middlewares/pathHandler.mid.js";
 import __dirname from "./utils.js";
-import morgan from "morgan";
 
 const server = express();
-
 const PORT = 8080;
 const ready = () => console.log("Servidor listo en puerto " + PORT);
+server.listen(PORT, ready);
+
+server.engine("handlebars", engine());
+server.set("view engine", "handlebars");
+server.set("views", __dirname + "/src/views");
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
@@ -17,5 +23,3 @@ server.use(morgan("dev"));
 server.use("/", router);
 server.use(errorHandler);
 server.use(pathHandler);
-
-server.listen(PORT, ready);
--- src/routers/api/index.router.js
+++ src/routers/api/index.router.js
@@ -1,14 +1,14 @@
 import { Router } from "express";
-import usersRouter from "./users.routers.js";
-import productsRouter from "./products.router.js";
-import ordersRouter from "./orders.router.js";
+// import usersRouter from "./users.routers.js";
+// import productsRouter from "./products.router.js";
+// import ordersRouter from "./orders.router.js";
 
 const apiRouter = Router();
 
-// definir los enrutadores de los recursos
-apiRouter.use("/users", usersRouter);
-apiRouter.use("/products", productsRouter);
-apiRouter.use("/orders", ordersRouter);
+// // definir los enrutadores de los recursos
+// apiRouter.use("/users", usersRouter);
+// apiRouter.use("/products", productsRouter);
+// apiRouter.use("/orders", ordersRouter);
 
 export default apiRouter;
-// exporto el enrutador de la api para poder implementarlo en el enrutador del servidor
+// // exporto el enrutador de la api para poder implementarlo en el enrutador del servidor
