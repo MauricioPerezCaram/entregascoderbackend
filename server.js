9a59cb7288d40dae553c98b6c5b4e8bd61db3aed
Mauricio Pérez <perezcarammauricio@hotmail.com>
2024-02-07 15:44:25 -0300

    cookies

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ import router from "./src/routers/index.router.js";
 import errorHandler from "./src/middlewares/errorHandler.mid.js";
 import pathHandler from "./src/middlewares/pathHandler.mid.js";
 import __dirname from "./utils.js";
+import cookieParser from "cookie-parser";
 
 const server = express();
 const PORT = process.env.PORT || 8080;
@@ -29,6 +30,7 @@ server.set("view engine", "handlebars");
 server.set("views", __dirname + "/src/views");
 
 //middlewares
+server.use(cookieParser(process.env.SECRET_KEY));
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use(express.static(__dirname + "/public"));
