07d5d1648cf68b6a2094ae029fbbbb09e67319af
Mauricio Pérez <perezcarammauricio@hotmail.com>
2024-02-21 18:22:47 -0300

    cambiar let response de createproduct.js

--- public/scripts/createproduct.js
+++ public/scripts/createproduct.js
@@ -1,1 +1,24 @@
-// ver handlebars por los #
+const selector = document.querySelector("#createproduct");
+selector.addEventListener("click", async () => {
+  try {
+    const data = {
+      nombre: document.querySelector("#nombre").value,
+      foto: document.querySelector("#foto").value,
+      precio: document.querySelector("#precio").value,
+      stock: document.querySelector("#stock").value,
+    };
+    // console.log(data);
+    const opts = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    };
+    let response = await fetch("/api/products/new", opts);
+    response = await response.json();
+    // console.log(response);
+    alert(response.message);
+    // response.session && location.replace("/sessions/login");
+  } catch (error) {
+    alert(error.message);
+  }
+});
