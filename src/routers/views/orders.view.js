393ab758bd2c169d54ec63688495c048d32671f2
Mauricio Pérez <perezcarammauricio@hotmail.com>
2024-02-26 14:02:36 -0300

    orders

--- src/routers/views/orders.view.js
+++ src/routers/views/orders.view.js
@@ -1,12 +1,45 @@
 import { Router } from "express";
-import orders from "../../data/fs/ordersManager.fs.js";
+
+import { orders } from "../../data/mongo/manager.mongo.js";
 
 const ordersRouter = Router();
 
 ordersRouter.get("/", async (req, res, next) => {
   try {
-    const all = await orders.readOrders();
-    return res.render("orders", { products: all });
+    const options = {
+      limit: req.query.limit || 12,
+      page: req.query.page || 1,
+      sort: { title: 1 },
+      lean: true,
+    };
+    const filter = {};
+    if (req.query.title) {
+      filter.title = new RegExp(req.query.title.trim(), "i");
+    }
+    if (req.query.sort === "desc") {
+      options.sort.title = "desc";
+    }
+    const all = await orders.read({ filter, options });
+    return res.render("orders", {
+      orders: all.docs,
+      next: all.nextPage,
+      prev: all.prevPage,
+      title: "ORDERS",
+      filter: req.query.title,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+ordersRouter.get("/:oid", async (req, res, next) => {
+  try {
+    const { oid } = req.params;
+    const one = await orders.readOne(oid);
+    return res.render("detail", {
+      order: one,
+      title: one.title.toUpperCase(),
+    });
   } catch (error) {
     next(error);
   }
