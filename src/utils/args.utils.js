86797b2d4b98a1ede672688ac74fbbafdac2ff72
Mauricio Pérez <perezcarammauricio@hotmail.com>
2024-03-07 14:41:28 -0300

    42

---
+++ src/utils/args.utils.js
@@ -0,0 +1,10 @@
+import { Command } from "commander";
+
+const args = new Command();
+
+args.option("-p <port>", "port");
+args.option("--env ", "enviroment", "prod");
+
+args.parse();
+
+export default args.opts();
