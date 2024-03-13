77bfb7924f6f802f6378ef7d7a115b02e5751ee8
Mauricio Pérez <perezcarammauricio@hotmail.com>
2024-03-13 19:53:24 -0300

    controllers listo para users products y orders

---
+++ src/controllers/orders.controller.js
@@ -0,0 +1,87 @@
+import { orders } from "../data/mongo/manager.mongo.js";
+
+class OrdersController {
+  constructor() {
+    this.model = orders;
+  }
+
+  create = async (req, res, next) => {
+    try {
+      const data = req.body;
+      const response = await orders.create(data);
+      return res.json({ statusCode: 201, response });
+    } catch (error) {
+      return next(error);
+    }
+  };
+
+  read = async (req, res, next) => {
+    try {
+      const options = {
+        limit: req.query.limit || 20,
+        page: req.query.page || 1,
+        sort: { title: 1 },
+        lean: true,
+      };
+      const filter = {};
+      if (req.query.title) {
+        filter.title = new RegExp(req.query.title.trim(), "i");
+      }
+      if (req.query.sort === "desc") {
+        options.sort.title = "desc";
+      }
+      const all = await orders.read({ filter, options });
+      return res.json({
+        statusCode: 200,
+        response: all,
+      });
+    } catch (error) {
+      return next(error);
+    }
+  };
+
+  readOne = async (req, res, next) => {
+    try {
+      const { pid } = req.params;
+      const one = await orders.readOne(pid);
+      return res.json({
+        statusCode: 200,
+        response: one,
+      });
+    } catch (error) {
+      return next(error);
+    }
+  };
+
+  update = async (req, res, next) => {
+    try {
+      const { pid } = req.params;
+      const data = req.body;
+      const response = await orders.update(pid, data);
+      return res.json({
+        statusCode: 200,
+        response: response,
+      });
+    } catch (error) {
+      return next(error);
+    }
+  };
+
+  destroy = async (req, res, next) => {
+    try {
+      const { pid } = req.params;
+      const response = await orders.destroy(pid);
+      return res.json({
+        statusCode: 200,
+        response,
+      });
+    } catch (error) {
+      return next(error);
+    }
+  };
+}
+
+export default OrdersController;
+const controller = new OrdersController();
+const { create, read, readOne, update, destroy } = controller;
+export { create, read, readOne, update, destroy };
