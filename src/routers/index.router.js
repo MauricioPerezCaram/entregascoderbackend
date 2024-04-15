26dc062a0e8589ec552e8bbec69ee72f21bfb365
Mauricio Pérez <perezcarammauricio@hotmail.com>
2024-04-15 20:13:14 -0300

    logger listo

--- src/routers/index.router.js
+++ src/routers/index.router.js
@@ -11,5 +11,19 @@ export default class IndexRouter extends CustomRouter {
   init() {
     this.router.use("/api", apiRouter);
     this.router.use("/", viewsRouter);
+    this.router.get("/simplex", (req, res) => {
+      let total = 1;
+      for (let i = 1; i < 100; i++) {
+        total = i * i;
+      }
+      return res.send({ total });
+    });
+    this.router.get("/complex", (req, res) => {
+      let total = 1;
+      for (let i = 1; i < 1000000000; i++) {
+        total = i * i;
+      }
+      return res.send({ total });
+    });
   }
 }
